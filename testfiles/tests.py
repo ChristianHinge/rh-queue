c42042d5912dce8c740cb2cbc8f31968f7b3cc35
Peter McDaniel <pmc@novicell.dk>
2020-05-26 15:11:26 +0200

    update

--- testfiles/tests.py
+++ testfiles/tests.py
@@ -28,7 +28,7 @@ class RHQueueTests(unittest.TestCase):
   def assertFileContentsSame(self, file, expected):
     val = ""
     while not os.path.isfile(file):
-      time.sleep(1)
+      time.sleep(3)
     with open(self.o, "r") as f:
       val = f.read().rstrip("\n")
     self.assertEqual(val, expected)
