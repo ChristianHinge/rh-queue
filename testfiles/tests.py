58189dbcbd600a346ee7d1b8d883ec74e80e5e1a
Peter McDaniel <pmc@novicell.dk>
2020-05-26 14:18:51 +0200

    update

--- testfiles/tests.py
+++ testfiles/tests.py
@@ -2,6 +2,7 @@ import unittest
 import subprocess
 import os
 import copy
+import inspect
 
 
 class RHQueueTests(unittest.TestCase):
@@ -22,26 +23,32 @@ class RHQueueTests(unittest.TestCase):
       ret.extend([f"-{val}", getattr(self, val)])
     return ret
 
+
   def test_create_file(self):
+    self.o = f"{inspect.currentframe().f_code.co_name}.stdout"
     script = subprocess.run(self.args("test_create_file.py"))
     self.assertEqual(script.returncode, 0)
     self.assertTrue(os.path.isfile("./new_file.txt"))
 
   def test_tensorflow(self):
+    self.o = f"{inspect.currentframe().f_code.co_name}.stdout"
     script = subprocess.run(self.args("test_tensorflow.py"))
     self.assertEqual(script.returncode, 0)
 
   def test_without_venv(self):
     val = copy.copy(self.base_args)
     val.remove("v")
+    self.o = f"{inspect.currentframe().f_code.co_name}.stdout"
     script = subprocess.run(self.args("test_venv.py", val))
     self.assertEqual(script.returncode, 0)
 
   def test_with_venv(self):
+    self.o = f"{inspect.currentframe().f_code.co_name}.stdout"
     script = subprocess.run(self.args("test_venv.py"))
     self.assertEqual(script.returncode, 0)
 
   def test_with_envvar_venv(self):
+    self.o = f"{inspect.currentframe().f_code.co_name}.stdout"
     val = copy.copy(self.base_args)
     val.remove("v")
     os.environ["RHQ_VENV"] = self.v
@@ -50,13 +57,13 @@ class RHQueueTests(unittest.TestCase):
     del os.environ["RHQ_VENV"]
 
   def test_begin_time(self):
+    self.o = f"{inspect.currentframe().f_code.co_name}.stdout"
     self.b = "1"
-    print(self.args("test_venv.py", self.base_args + ["b"]))
     script = subprocess.run(self.args("test_venv.py", self.base_args + ["b"]))
     self.assertEqual(script.returncode, 0)
 
   def test_output_file(self):
-    self.o = "test.stdout"
+    self.o = "test_output_file.stdout"
     script = subprocess.run(self.args("test_venv.py"))
     self.assertEqual(script.returncode, 0)
     self.assertTrue(os.path.isfile(self.o))
@@ -65,4 +72,5 @@ class RHQueueTests(unittest.TestCase):
 if __name__ == "__main__":
   test = RHQueueTests()
   print(test.args(""))
-  unittest.main()
+  print(test.get_function_name())
+  # unittest.main()
