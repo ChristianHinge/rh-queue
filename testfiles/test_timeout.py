33f132e2bf2b4fa6428f76a1be01aae4eb62b04a
Peter McDaniel <kg.mcd@hotmail.com>
2020-06-09 13:16:49 +0200

    update

---
+++ testfiles/test_timeout.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import time
+print("start")
+time.sleep(15)
+print("end")
---
+++ testfiles/test_venv_venv.py
@@ -0,0 +1,3 @@
+#!/homes/pmcd/venv/test-slurm/bin/python3
+import os
+print(os.environ.get("VIRTUAL_ENV"))
