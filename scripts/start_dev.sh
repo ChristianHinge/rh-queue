f1ff53752f55b9d70437dfd74dbac9ccc1183640
Peter McDaniel <kg.mcd@hotmail.com>
2021-02-23 14:35:56 +0100

    fixed install script

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -2,7 +2,7 @@ rhqinstall() {
   cd /homes/pmcd/rh-queue
   rm -rf ./build
   python3 setup.py bdist_wheel -d /opt/rh-queue
-  sudo -H -u root python3 -m pip install --upgrade /opt/rh-queue/rhqueue-1.1-py3-none-any.whl --no-cache
+  sudo -H -u root python3 -m pip install --upgrade /opt/rh-queue/*.whl --no-cache
   rm /homes/pmcd/.local/bin/rhqueue
   rm /homes/pmcd/.local/bin/rhqemail
   rm /opt/rh-queue/*.whl
